12ac7a86a2d8417c507ca0a6b14453cc6fb85ea0
dareodutola <dareodutola@gmail.com>
2026-03-10 13:14:41 +0100

    Update index.js
    dre

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Dare Updated this file just now\n');
+  res.end('Doctor dre Updated this file just now\n');
 });
 
 server.listen(port, () => {
